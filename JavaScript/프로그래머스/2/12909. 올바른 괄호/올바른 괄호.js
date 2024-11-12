a85de79388f91d277bb18a10e6af64670d887604
@u_limit_ <76727686+urimeee@users.noreply.github.com>
2024-11-12 17:02:35 +0900

    [level 2] Title: 올바른 괄호, Time: 33.91 ms, Memory: 37.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,28 +1,13 @@
 function solution(s){ 
-    var answer = true;
-    let s1 = s.split('');
-
-    if(s[0] === "(" ){
-        if(s[s.length-1] === "("){
-                return false;
-        }
-    }
-    
-    else if (s[0] === ")") {
-        return false;
-    }
-    let length = s1.length;
     let stack = [];
-    // 닫는 괄호들로만 이루어져 있음
-    for(let i=0; i < length; i++){
-        if(s1[i] === "(") {
-            stack.push(s1[i]);
+    for(let i=0; i < s.length; i++){
+        if(s[i] === "(") {
+            stack.push(s[i]);
         }
         else {
+            if(stack.length === 0) return false;
             stack.pop();
         }
     }
-    if(stack.length !=0) return false;
-    return true;
-  
+    return stack.length === 0;
 }
