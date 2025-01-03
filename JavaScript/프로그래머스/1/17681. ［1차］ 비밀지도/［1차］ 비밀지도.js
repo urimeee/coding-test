6f6f6a80add96d172c4b02151a7b79f9cac5e5d4
@u_limit_ <76727686+urimeee@users.noreply.github.com>
2025-01-04 00:23:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,24 +1,16 @@
 function solution(n, arr1, arr2) {
     let answer = [];
     
-    // 각 배열에 대해 이진수로 변환하고, n길이에 맞게 패딩 추가
-    let newArray1 = arr1.map((arr) => {
-        return arr.toString(2).padStart(n, '0');
-    });
-    
-    let newArray2 = arr2.map((arr) => {
-        return arr.toString(2).padStart(n, '0');
-    });
-    
-    // 두 배열의 이진수 OR 연산을 통해 비밀 지도 생성
     for (let i = 0; i < n; i++) {
         let row = "";
-        
-        for (let j = 0; j < n; j++) {
-            // 두 지도에서 해당 자리의 이진수 값 비교 후, OR 연산
-            if (newArray1[i][j] === '1' || newArray2[i][j] === '1') {
+        let result = arr1[i] | arr2[i];
+        // 11101
+        // 밑에서부터는 각 자리가 1인지 아닌지 판단해야한다.
+        for (let j = n-1 ; j>=0; j--){
+            if(result&(1 << j)){
                 row += "#";
-            } else {
+            }
+            else {
                 row += " ";
             }
         }
