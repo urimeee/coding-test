d0615c861250abb172c17172a7db7bb521c02c84
@u_limit_ <76727686+urimeee@users.noreply.github.com>
2025-03-24 22:56:01 +0900

    [level 2] Title: 주식가격, Time: 5.59 ms, Memory: 44.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42584. 주식가격/주식가격.js
+++ JavaScript/프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,21 +1,18 @@
 function solution(prices) {
-    let stack = []; // 값 말고 인덱스를 저장한다.
-    let answer = Array(prices.length).fill(0);
+    var answer = new Array(prices.length).fill(0);
     
-    for (let i = 0; i < prices.length; i++) {
-        //stack의 길이가 0이상이고 현재 가격이 스택의 맨위 가격보다 낮을 때 (떨어졌을 때)
-        while(stack.length > 0 && prices[stack[stack.length-1]] > prices[i]){
-                let temp = stack.pop();
-                answer[temp] = i - temp;            
+    for (let i = 0; i < prices.length; i++){
+        let temp = 0;
+        
+        for (let j = i+1; j < prices.length; j++){
+            temp ++;
+            if (prices[i] > prices[j]) {
+                answer[i] = temp;
+                break;
+            }
+            answer[i] = temp;
 
         }
-                stack.push(i); // 현재 인덱스 저장한다
-
-    }
-    
-    while(stack.length >0 ) {
-        let temp = stack.pop();
-        answer[temp] = prices.length - temp -1 ;
     }
     
     return answer;
