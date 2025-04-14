069774aeca4ec40e1c5d990accb283334088125b
@u_limit_ <76727686+urimeee@users.noreply.github.com>
2025-04-14 19:26:06 +0900

    [level 2] Title: 기능개발, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42586. 기능개발/기능개발.js
+++ JavaScript/프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,19 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let count = 0;
-    const progressLength = progresses.length;
+    let leftProgresses = progresses.map((progress, i) => (Math.ceil((100-progress)/speeds[i])));
+    let deployDate = leftProgresses[0];
     
-    const remaining = progresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
-    let deployDate = remaining[0];
-
-
-    for (let i = 0; i < remaining.length; i++) {
-        if (remaining[i] <= deployDate) {
-            count ++;
+    let temp = 0;
+    for (let i = 0; i < leftProgresses.length; i++) {
+        if (leftProgresses[i] <= deployDate) {
+            temp ++;
         }
         else {
-            answer.push(count);
-            deployDate = remaining[i];
-            count = 1;
+            answer.push(temp);
+            deployDate = leftProgresses[i]
+            temp = 1;
         }
     }
-    answer.push(count);
+    answer.push(temp);
     return answer;
 }
